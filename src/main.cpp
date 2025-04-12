77797fc7153aea57a07b4bb6337dc96a6b948a39
Kyra Yee <yeekyra@gmail.com>
2025-04-12 09:12:59 -0700

    Simple test to write to GPIO1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-
+const int GPIO1 = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Hello, world!");
   Serial.println("This is a test of the serial communication.");
-  
+  pinMode(GPIO1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello, world!");
+  digitalWrite(GPIO1, HIGH);
   delay(1000); // Wait for a second
 }
 
